a9fe3f0547cf0257fadb14a13b9adda8b1080a07
Kwonyujin <tina90808@gmail.com>
2024-05-31 19:49:45 +0900

    Update customwidget.cpp

--- p665/customwidget.cpp
+++ p665/customwidget.cpp
@@ -6,7 +6,7 @@ CustomWidget::CustomWidget(QWidget *parent) : QWidget(parent)
 {
     QPushButton *button = new QPushButton("Quit", this);
     button->resize(180, 35); 		/* 위젯의 크기 설정 */
-    button->setText("TEST Button");
+    button->setText("BUTTON");
 	button->show();
 	this->resize(200,200);
     move(300, 300);		/* 위젯의 위치 설정 */
